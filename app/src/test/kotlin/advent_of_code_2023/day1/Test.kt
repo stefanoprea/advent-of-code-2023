d8c7a15d9e6aa5a9999234f9219afe932d6ca945
Stefan <stefanoprea128@gmail.com>
2023-12-01 16:25:20 +0200

    day1

--- app/src/test/kotlin/advent_of_code_2023/day1/Test.kt
+++ app/src/test/kotlin/advent_of_code_2023/day1/Test.kt
@@ -11,26 +11,45 @@ class MyTest {
     @Test
     fun A() {
         assertEquals(
-            "",
-            Days.getInput(day).let {
-                it
-                    .trim('\n')
-                    .lines()
-                    .first()
-            },
+            "54597",
+            Days.getInput(day).solve(digits),
         )
     }
 
     @Test
     fun B() {
         assertEquals(
-            "",
-            Days.getInput(day).let {
-                it
-                    .trim('\n')
-                    .lines()
-                    .first()
-            },
+            "54504",
+            Days.getInput(day).solve(digits + words),
         )
     }
 }
+
+fun (String).solve(L: List<Pair<String, Int>>) = this
+    .trim('\n')
+    .lines()
+    .map { it
+        .substrings()
+        .mapNotNull { substring ->
+            L.firstNotNullOfOrNull { (k, v) -> v.takeIf { substring.startsWith(k) } }
+        }
+        .run { first() * 10 + last() }
+    }
+    .sum()
+    .toString()
+
+fun (String).substrings() = (0 ..< length).map { substring(it) }
+
+val digits: List<Pair<String, Int>> = (0..9).map { it.toString() to it }
+
+val words: List<Pair<String, Int>> = listOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
